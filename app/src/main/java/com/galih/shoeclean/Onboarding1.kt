33afb1965cc908582d4405a137118f62a3258cc7
DimasAsia <galihdimas@outlook.com>
2023-10-13 09:04:42 +0700

    feature - onboarding page 1 dan 2 update code dan connect page

--- app/src/main/java/com/galih/shoeclean/Onboarding1.kt
+++ app/src/main/java/com/galih/shoeclean/Onboarding1.kt
@@ -1,11 +1,23 @@
 package com.galih.shoeclean
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class Onboarding1 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splashscreen)
+        setContentView(R.layout.activity_onboarding1)
+
+        var button: ImageButton = findViewById(R.id.btn_arrow)
+        button.setOnClickListener(){
+            var intent = Intent(this@Onboarding1, Onboarding2::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/galih/shoeclean/SplashScreen.kt
+++ app/src/main/java/com/galih/shoeclean/SplashScreen.kt
@@ -1,11 +1,20 @@
 package com.galih.shoeclean
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
+
+        var handler = Handler()
+        handler.postDelayed({
+            var intent = Intent(this@SplashScreen, Onboarding1::class.java)
+            startActivity(intent)
+            finish()
+        }, 5000)
     }
 }
